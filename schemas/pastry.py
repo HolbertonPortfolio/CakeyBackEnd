380068fa81e13713e101537624f04a4e276fc769
isaquliyev <isa.quluyev.wip@gmail.com>
2024-05-17 19:44:30 +0400

    Validator for pastry name

--- schemas/pastry.py
+++ schemas/pastry.py
@@ -1,8 +1,14 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 
 class PastryBase(BaseModel):
     name: str
+
+    @validator('name')
+    def name_must_not_be_empty(cls, v):
+        if not v or not v.strip():
+            raise ValueError('Name must not be empty')
+        return v
 
 
 class PastryCreate(PastryBase):
