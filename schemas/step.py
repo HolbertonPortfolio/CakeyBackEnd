143681cc433229e671603cf565b366ed57581ecf
ilaahe04 <ilaha.hybtv04@gmail.com>
2024-06-10 19:40:39 +0400

    Change one to one relationship

--- schemas/step.py
+++ schemas/step.py
@@ -4,11 +4,18 @@ from pydantic import BaseModel, validator
 class StepBase(BaseModel):
     description: str
     timer: int
+    step_number: int
 
     @validator('description')
     def description_must_not_be_empty(cls, v):
         if not v.strip():
             raise ValueError('Description must not be empty')
+        return v
+
+    @validator('step_number')
+    def step_number_must_be_positive(cls, v):
+        if v <= 0:
+            raise ValueError('Step number must be a positive integer')
         return v
 
 
